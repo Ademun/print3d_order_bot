01d551d0351f4c6dacf80c0c98de5568be80a779
Ademun <78663343+Ademun@users.noreply.github.com>
2025-12-16 18:59:18 +0300

    New errors

--- internal/file/errors.go
+++ internal/file/errors.go
@@ -26,3 +26,19 @@ type ErrPrepareFilepath struct {
 func (e *ErrPrepareFilepath) Error() string {
 	return fmt.Errorf("failed to prepare file path: %w", e.Err).Error()
 }
+
+type ErrReadDir struct {
+	Err error
+}
+
+func (e *ErrReadDir) Error() string {
+	return fmt.Errorf("failed to read directory: %w", e.Err).Error()
+}
+
+type ErrOpenFile struct {
+	Err error
+}
+
+func (e *ErrOpenFile) Error() string {
+	return fmt.Errorf("failed to open file: %w", e.Err).Error()
+}
