9f0dc57fc2333b8a951edb5753dc5785c0b35c75
Ademun <78663343+Ademun@users.noreply.github.com>
2026-01-03 23:49:01 +0300

    Bot Api downloader

--- internal/file/downloader.go
+++ internal/file/downloader.go
@@ -2,8 +2,12 @@ package file
 
 import (
 	"context"
+	"fmt"
 	"io"
+	"net/http"
 	"print3d-order-bot/internal/mtproto"
+
+	"github.com/go-telegram/bot"
 )
 
 type Downloader interface {
@@ -22,4 +26,40 @@ func NewMTProtoDownloader(client *mtproto.Client) *MTProtoDownloader {
 
 func (d *MTProtoDownloader) DownloadFile(ctx context.Context, fileID string, dst io.Writer) error {
 	return d.client.DownloadFile(ctx, fileID, dst)
+}
+
+type BotApiDownloader struct {
+	bot *bot.Bot
+}
+
+func NewBotApiDownloader(bot *bot.Bot) *BotApiDownloader {
+	return &BotApiDownloader{
+		bot: bot,
+	}
+}
+
+func (d *BotApiDownloader) DownloadFile(ctx context.Context, fileID string, dst io.Writer) error {
+	file, err := d.bot.GetFile(ctx, &bot.GetFileParams{
+		FileID: fileID,
+	})
+	if err != nil {
+		return err
+	}
+
+	link := d.bot.FileDownloadLink(file)
+	resp, err := http.Get(link)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	if _, err := io.Copy(dst, resp.Body); err != nil {
+		return err
+	}
+
+	return nil
 }
