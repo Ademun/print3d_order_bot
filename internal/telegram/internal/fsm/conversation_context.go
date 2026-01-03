e349b18517fb119318155844b4385848b91c12f6
Ademun <78663343+Ademun@users.noreply.github.com>
2026-01-03 13:32:28 +0300

    ConversationContext struct

---
+++ internal/telegram/internal/fsm/conversation_context.go
@@ -0,0 +1,37 @@
+package fsm
+
+import (
+	"context"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+type ConversationContext[T StateData] struct {
+	Ctx    context.Context
+	Bot    *bot.Bot
+	Update *models.Update
+	UserID int64
+	Data   T
+	fsm    *FSM
+	step   ConversationStep
+}
+
+func (c *ConversationContext[T]) SendMessage(text string, markup *models.ReplyMarkup) error {
+	params := &bot.SendMessageParams{
+		ChatID:      c.UserID,
+		Text:        text,
+		ParseMode:   models.ParseModeHTML,
+		ReplyMarkup: markup,
+	}
+	_, err := c.Bot.SendMessage(c.Ctx, params)
+	return err
+}
+
+func (c *ConversationContext[T]) Transition(nextStep ConversationStep, data StateData) error {
+	return c.fsm.Transition(c.Ctx, c.UserID, nextStep, data)
+}
+
+func (c *ConversationContext[T]) Complete() error {
+	return c.fsm.ResetState(c.Ctx, c.UserID)
+}
