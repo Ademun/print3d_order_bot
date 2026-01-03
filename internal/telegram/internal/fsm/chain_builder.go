6b4b851a1458021f65212dd9b08a3b4298aad398
Ademun <78663343+Ademun@users.noreply.github.com>
2026-01-03 13:46:35 +0300

    ChainBuilder implementation

---
+++ internal/telegram/internal/fsm/chain_builder.go
@@ -0,0 +1,82 @@
+package fsm
+
+import "fmt"
+
+type UniversalHandler[T StateData] func(*ConversationContext[T]) error
+
+type TextHandler[T StateData] func(*ConversationContext[T], string) error
+
+type CallbackHandler[T StateData] func(*ConversationContext[T], string) error
+
+func Chain[T StateData](router *Router, name string, initialStep ConversationStep) *ChainDefinition[T] {
+	return &ChainDefinition[T]{
+		name:    name,
+		router:  router,
+		current: initialStep,
+	}
+}
+
+type ChainDefinition[T StateData] struct {
+	name    string
+	router  *Router
+	current ConversationStep
+}
+
+func (c *ChainDefinition[T]) OnText(handler TextHandler[T]) *ChainDefinition[T] {
+	wrapped := func(ctx *ConversationContext[StateData]) error {
+		typedData, ok := ctx.Data.(T)
+		if !ok {
+			return fmt.Errorf("type assertion failed")
+		}
+		typedCtx := &ConversationContext[T]{
+			Ctx:    ctx.Ctx,
+			Bot:    ctx.Bot,
+			Update: ctx.Update,
+			UserID: ctx.UserID,
+			Data:   typedData,
+			fsm:    ctx.fsm,
+			step:   ctx.step,
+		}
+
+		if typedCtx.Update.Message == nil {
+			return nil
+		}
+
+		return handler(typedCtx, typedCtx.Update.Message.Text)
+	}
+
+	c.router.RegisterHandler(c.current, wrapped)
+	return c
+}
+
+func (c *ChainDefinition[T]) OnCallback(handler CallbackHandler[T]) *ChainDefinition[T] {
+	wrapped := func(ctx *ConversationContext[StateData]) error {
+		typedData, ok := ctx.Data.(T)
+		if !ok {
+			return fmt.Errorf("type assertion failed")
+		}
+		typedCtx := &ConversationContext[T]{
+			Ctx:    ctx.Ctx,
+			Bot:    ctx.Bot,
+			Update: ctx.Update,
+			UserID: ctx.UserID,
+			Data:   typedData,
+			fsm:    ctx.fsm,
+			step:   ctx.step,
+		}
+
+		if typedCtx.Update.CallbackQuery == nil {
+			return nil
+		}
+
+		return handler(typedCtx, typedCtx.Update.CallbackQuery.Data)
+	}
+
+	c.router.RegisterHandler(c.current, wrapped)
+	return c
+}
+
+func (c *ChainDefinition[T]) Then(nextStep ConversationStep) *ChainDefinition[T] {
+	c.current = nextStep
+	return c
+}
